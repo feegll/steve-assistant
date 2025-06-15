0dd490558fa24d389b8fd125479c78ab83876d11
feegll <lilmikolya@gmail.com>
2025-06-16 01:38:32 +0300

    Update log.py

--- steve/log.py
+++ steve/log.py
@@ -1,1 +1,15 @@
+import json
+import os
+
+LOG_PATH = "log_sent.json"
+
+def load_log():
+    if os.path.exists(LOG_PATH):
+        with open(LOG_PATH, "r") as f:
+            return json.load(f)
+    return {}
+
+def save_log(log):
+    with open(LOG_PATH, "w") as f:
+        json.dump(log, f)
 
