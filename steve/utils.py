263d3648e65dc1fe0c568e7ef6f675b9120b660c
feegll <lilmikolya@gmail.com>
2025-06-16 00:39:30 +0300

    Update utils.py

--- steve/utils.py
+++ steve/utils.py
@@ -1,1 +1,13 @@
-
+def extract_context(lines):
+    """
+    Преобразует строки из Telegram в структуру данных.
+    """
+    context = {
+        "first_name": lines[0].strip(),
+        "birth_date": lines[1].strip(),
+        "phone": lines[2].strip(),
+        "start_date": lines[3].strip(),
+        "shift": lines[4].strip(),
+        "username": lines[5].strip() if len(lines) >= 6 else None,
+    }
+    return context
